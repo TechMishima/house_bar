66c1bad84a3a73104103a6efcc7c5a817478c81a
TechMishima <t.mishima242@gmail.com>
2023-08-08 14:10:53 +0900

    userマイグレーション実行

--- db/migrate/20230808050040_devise_create_users.rb
+++ db/migrate/20230808050040_devise_create_users.rb
@@ -7,6 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :nickname, null: false
+      t.string :bar
+      t.text   :bio
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
