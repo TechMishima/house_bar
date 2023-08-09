fa092cb319e7edd6b4213fea2767dc72e88f54f4
TechMishima <t.mishima242@gmail.com>
2023-08-09 21:41:57 +0900

    ルーティング設定

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,25 @@
 class CocktailsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :description, :base_id, :type_id, :ingredient, :tool, :recipe)
+  end
+
 end
