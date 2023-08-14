c0297f19d9df966aefd60c3844b43fa10b66c9d9
TechMishima <t.mishima242@gmail.com>
2023-08-14 15:11:20 +0900

    詳細ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    # binding.pry
+    @user = User.find(params[:id])
+    @cocktails = Cocktail.where(user_id: @user.id).order(created_at: :desc).limit(4)
   end
 end
